ac685361b314fba02e8d6cc4b1e508904b7c2ef9
Raphael <raphael.temperli@epfl.ch>
2023-03-29 17:13:28 +0200

    update pinout

--- test_steppers_and_encoders_directions/test_steppers_and_encoders_directions.ino
+++ test_steppers_and_encoders_directions/test_steppers_and_encoders_directions.ino
@@ -14,21 +14,21 @@
 #define High(pin) digitalWrite(pin, HIGH)
 #define Low(pin) digitalWrite(pin, LOW)
 
-// #define STEPPER_ALT_ENABLE_PIN 21
-// #define STEPPER_ALT_DIR_PIN 22
-// #define STEPPER_ALT_STEP_PIN 23
-// #define STEPPER_ALT_BOOST_PIN 20
-// #define STEPPER_ALT_FAULT_PIN 19
+#define STEPPER_ALT_STEP_PIN 25
+#define STEPPER_ALT_DIR_PIN 26
+#define STEPPER_ALT_ENABLE_PIN 27
+#define STEPPER_ALT_BOOST_PIN 14
+#define STEPPER_ALT_FAULT_PIN 12
 
-// #define ENCODER_ALT_NCS_PIN 15
+#define ENCODER_ALT_NCS_PIN 21
 
-#define STEPPER_AZ_ENABLE_PIN 14
-#define STEPPER_AZ_DIR_PIN 18
-#define STEPPER_AZ_STEP_PIN 20
-// #define STEPPER_AZ_BOOST_PIN 1
-// #define STEPPER_AZ_FAULT_PIN 0
+#define STEPPER_AZ_STEP_PIN 16
+#define STEPPER_AZ_DIR_PIN 4
+#define STEPPER_AZ_ENABLE_PIN 0
+#define STEPPER_AZ_BOOST_PIN 2
+#define STEPPER_AZ_FAULT_PIN 15
 
- #define ENCODER_AZ_NCS_PIN 9
+#define ENCODER_AZ_NCS_PIN 22
 
 void setup()
 {
@@ -44,26 +44,28 @@ void setup()
     pinMode(STEPPER_AZ_ENABLE_PIN, OUTPUT);
     pinMode(STEPPER_AZ_DIR_PIN, OUTPUT);
     pinMode(STEPPER_AZ_STEP_PIN, OUTPUT);
-    // pinMode(STEPPER_AZ_BOOST_PIN, OUTPUT);
-    // pinMode(STEPPER_AZ_FAULT_PIN, INPUT_PULLUP);
+    pinMode(STEPPER_AZ_BOOST_PIN, OUTPUT);
+    pinMode(STEPPER_AZ_FAULT_PIN, INPUT_PULLUP);
 
     High(STEPPER_AZ_ENABLE_PIN);
     High(STEPPER_AZ_DIR_PIN);
 
     pinMode(ENCODER_AZ_NCS_PIN, OUTPUT);
+    High(ENCODER_AZ_NCS_PIN);
 
     // init alt
 
-    // pinMode(STEPPER_ALT_ENABLE_PIN, OUTPUT);
-    // pinMode(STEPPER_ALT_DIR_PIN, OUTPUT);
-    // pinMode(STEPPER_ALT_STEP_PIN, OUTPUT);
-    // pinMode(STEPPER_ALT_BOOST_PIN, OUTPUT);
-    // pinMode(STEPPER_ALT_FAULT_PIN, INPUT_PULLUP);
+    pinMode(STEPPER_ALT_ENABLE_PIN, OUTPUT);
+    pinMode(STEPPER_ALT_DIR_PIN, OUTPUT);
+    pinMode(STEPPER_ALT_STEP_PIN, OUTPUT);
+    pinMode(STEPPER_ALT_BOOST_PIN, OUTPUT);
+    pinMode(STEPPER_ALT_FAULT_PIN, INPUT_PULLUP);
 
-    // High(STEPPER_ALT_ENABLE_PIN);
-    // High(STEPPER_ALT_DIR_PIN);
+    High(STEPPER_ALT_ENABLE_PIN);
+    High(STEPPER_ALT_DIR_PIN);
 
-    // pinMode(ENCODER_ALT_NCS_PIN, OUTPUT);
+    pinMode(ENCODER_ALT_NCS_PIN, OUTPUT);
+    High(ENCODER_ALT_NCS_PIN);
 
     // for (int i = 0; i < 200; i++)
     // {
@@ -79,13 +81,13 @@ void setup()
 
 void loop()
 {
-    //encoders_print();
+    encoders_print();
     
     //delay(500);
 
-    step_forward_az();
+    //step_forward_az();
 
-    HWSerial.println("eeeeeeeee");
+    //HWSerial.println("eeeeeeeee");
 }
 
 // todo draft:
