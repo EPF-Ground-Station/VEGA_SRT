73f426e9ce30f2f11fbe2aeba8d4552643e464b0
Raphael <raphael.temperli@epfl.ch>
2023-03-31 18:54:05 +0200

    complete documentation

--- documentation.md
+++ documentation.md
@@ -27,11 +27,20 @@ UART to USB converter
 	  black ENABLE
 	  gray	GND ?
 	  
-min STEP interval tested = 100ms
+min az driver step interval tested = 20us
+min alt driver step interval tested = 30us (20us too low)
   
+  az 12800 microstep / rev and 200 reduction
+  alt 25600 microstep / rev and 140 reduction
   
   alt with pin dir high gets down
   az with pin dir high turn clockwise when looking from above
   
+  encoder alt lowering decreasing
+  encoder az  turn clockwise (looking from above) increase
+  
  for encoders use GPIO instead of builtin SPI CS (doesn't work for some reason else)
  
+ to program the esp32 (model https://www.elektronik-kaufen.ch/products/nodemcu32?variant=41643637047472&currency=CHF)
+ 	disconnect rx and tx of hardware serial cable
+ 	press boot mode button while programming
--- test_steppers_and_encoders_directions/test_steppers_and_encoders_directions.ino
+++ test_steppers_and_encoders_directions/test_steppers_and_encoders_directions.ino
@@ -6,13 +6,16 @@
 
 #define ZERO 0x00
 
-#define STEP_DURATION_AZ_MS 100
-#define STEP_DURATION_ALT_MS 100
+#define STEP_DURATION_AZ_MS 20
+#define STEP_DURATION_ALT_MS 30
 
-// #define High(pin) digitalWriteFast(pin, HIGH)
-// #define Low(pin) digitalWriteFast(pin, LOW)
+//#define High(pin) digitalWriteFast(pin, HIGH)
+//#define Low(pin) digitalWriteFast(pin, LOW)
 #define High(pin) digitalWrite(pin, HIGH)
 #define Low(pin) digitalWrite(pin, LOW)
+
+//#define High(pin) GPIO.out_w1ts = ((uint32_t)1 << pin);
+//#define Low(pin) GPIO.out_w1tc = ((uint32_t)1 << pin);
 
 #define STEPPER_ALT_STEP_PIN 25
 #define STEPPER_ALT_DIR_PIN 26
@@ -25,8 +28,8 @@
 #define STEPPER_AZ_STEP_PIN 17
 #define STEPPER_AZ_DIR_PIN 16
 #define STEPPER_AZ_ENABLE_PIN 4
-#define STEPPER_AZ_BOOST_PIN 2
-#define STEPPER_AZ_FAULT_PIN 15
+#define STEPPER_AZ_BOOST_PIN 0
+#define STEPPER_AZ_FAULT_PIN 2
 
 #define ENCODER_AZ_NCS_PIN 22
 
@@ -48,7 +51,7 @@ void setup()
     pinMode(STEPPER_AZ_FAULT_PIN, INPUT_PULLUP);
 
     High(STEPPER_AZ_ENABLE_PIN);
-    Low(STEPPER_AZ_DIR_PIN);
+    High(STEPPER_AZ_DIR_PIN);
 
     pinMode(ENCODER_AZ_NCS_PIN, OUTPUT);
     High(ENCODER_AZ_NCS_PIN);
@@ -67,35 +70,42 @@ void setup()
     pinMode(ENCODER_ALT_NCS_PIN, OUTPUT);
     High(ENCODER_ALT_NCS_PIN);
 
-    HWSerial.println("begin turn forward az");
 
+    
 
-    for (int i = 0; i < 200; i++)
+    // HWSerial.println("begin turn forward az");
+
+    // for (int i = 0; i < 200*200*40; i++)
+    // {
+    //     step_forward_az();
+    // }
+
+    // HWSerial.println("finished turn forward az");
+
+    HWSerial.println("begin quarter turn forward alt");
+
+    for (int i = 0; i < 140*25600/4; i++)
     {
-        HWSerial.println("step az ");
-        step_forward_az();
-    }
-
-    HWSerial.println("finished turn forward alt");
-
-
-    for (int i = 0; i < 200; i++)
-    {
-        HWSerial.println("step alt ");
         step_forward_alt();
     }
 
-    HWSerial.println("finished turn forward alt");
+    HWSerial.println("finished quarter turn forward alt");
 }
+
+int i = 0;
 
 void loop()
 {
-    encoders_print();
-    delay(500);
+    // encoders_print();
+    // delay(500);
 
-    //step_forward_az();
 
-    //HWSerial.println("eeeeeeeee");
+    // step_forward_alt();
+    // i++;
+
+    // if( (i % 100) == 0){
+    //     HWSerial.println(i);
+    // }
 }
 
 // todo draft:
@@ -127,39 +137,36 @@ void encoders_print()
 
     //-------------- ALT ------------
 
-    // Low(ENCODER_ALT_NCS_PIN);
+    Low(ENCODER_ALT_NCS_PIN);
 
-    // delayMicroseconds(10);
+    delayMicroseconds(10);
 
-    // first_word = SPI.transfer16(ZERO);
-    // second_word = SPI.transfer16(ZERO);
-    // third_word = SPI.transfer16(ZERO);
+    second_word = SPI.transfer16(ZERO);
+    third_word = SPI.transfer16(ZERO);
 
-    // High(ENCODER_ALT_NCS_PIN);
+    High(ENCODER_ALT_NCS_PIN);
 
-    // pos_dec = (second_word << 4) + (third_word >> 12);
-    // pos_deg = (pos_dec * 360.0) / ENCODERS_MAX;
+    pos_dec = (second_word << 4) + (third_word >> 12);
+    pos_deg = (pos_dec * 360.0) / ENCODERS_MAX;
 
-    // HWSerial.print("alt encoder deg ");
-    // HWSerial.println(pos_deg);
-    // HWSerial.print("alt encoder first word (also turn counter ?)");
-    // HWSerial.println(first_word);
+    HWSerial.print("alt encoder deg ");
+    HWSerial.println(pos_deg);
 }
 
 void step_forward_az()
 {
 
     High(STEPPER_AZ_STEP_PIN);
-    delay(STEP_DURATION_AZ_MS / 2);
+    delayMicroseconds(STEP_DURATION_AZ_MS / 2);
     Low(STEPPER_AZ_STEP_PIN);
-    delay(STEP_DURATION_AZ_MS / 2);
+    delayMicroseconds(STEP_DURATION_AZ_MS / 2);
 }
 
 void step_forward_alt()
 {
 
     High(STEPPER_ALT_STEP_PIN);
-    delay(STEP_DURATION_ALT_MS / 2);
+    delayMicroseconds(STEP_DURATION_ALT_MS / 2);
     Low(STEPPER_ALT_STEP_PIN);
-    delay(STEP_DURATION_ALT_MS / 2);
+    delayMicroseconds(STEP_DURATION_ALT_MS / 2);
 }
