51113cac6329764d2f74b14f6a07d4e518b2831c
dhollosi <hollodimi@gmail.com>
2023-07-17 18:35:07 +0200

    updates..

--- GS_interface/point_to.py
+++ GS_interface/point_to.py
@@ -13,11 +13,10 @@ def name_input(name):
 
 def parse_azel_coordinates():
     # Regular expression to validate the input format
-    coord_pattern = re.compile(r'^[0-9]{1,2}\s[0-9]{1,2}\s[0-9]+\.[0-9]+$')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-az', nargs=3, type=valid_float, metavar=('HH', 'MM', 'SS.SSSS'), help='Right Ascension (hh mm ss.ss)')
-    parser.add_argument('-d', nargs=3, type=valid_float, metavar=('DD', 'MM', 'SS.SSSS'), help='Declination (dd mm ss.ss)')
+    parser.add_argument('-az', nargs=1, type=valid_float, metavar=('Deg'), help='Azimuth angle (degrees)')
+    parser.add_argument('-el', nargs=1, type=valid_float, metavar=('Deg'), help='Elevation angle (degrees)')
     args = parser.parse_args()
 
     # Extracting the values from the input
@@ -55,7 +54,7 @@ if __name__ == "__main__":
     az_angle, el_angle = parse_azel_coordinates()
     az_angle_str = string(az_angle)
     el_angle_str = string(el_angle)
-    coords_str = [az_angle_str el_angle_str]
+    coords_str = [az_angle_str, el_angle_str]
     #print("Rightascencion as Decimal number = ", ra)
     #print("Declination as Decimal number = ", dec)
     #coords_altaz = transform_skycoord_to_AltAz(ra, dec)
