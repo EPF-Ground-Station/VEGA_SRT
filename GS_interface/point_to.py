eee1073a0157f27efc3b31fbf0bbcf460ddf9b10
dhollosi <hollodimi@gmail.com>
2023-07-17 18:41:48 +0200

    ...

--- GS_interface/point_to.py
+++ GS_interface/point_to.py
@@ -43,7 +43,8 @@ def send_coord(coord, ser):
             if DEBUG:
                 print(coord)
 
-            ser.write(("point_to " + coord).encode())
+            #ser.write(("point_to " + coord).encode())
+            ser.write(("point_to 10 10").encode())
             print("wrote")
 
             # print(ser.readline().decode('utf-8'))
@@ -52,9 +53,9 @@ def send_coord(coord, ser):
      
 if __name__ == "__main__":
     az_angle, el_angle = parse_azel_coordinates()
-    az_angle_str = string(az_angle)
-    el_angle_str = string(el_angle)
-    coords_str = [az_angle_str, el_angle_str]
+    az_angle_str = str(az_angle)
+    el_angle_str = str(el_angle)
+    coords_str = [az_angle_str + el_angle_str]
     #print("Rightascencion as Decimal number = ", ra)
     #print("Declination as Decimal number = ", dec)
     #coords_altaz = transform_skycoord_to_AltAz(ra, dec)
