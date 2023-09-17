0dbf9661ba993faf41afc88024bddeacf82f7f02
Lgtlebrun <leonard.lebrun@epfl.ch>
2023-09-17 20:25:03 +0200

    iss

--- GS_interface/testISS.py
+++ GS_interface/testISS.py
@@ -10,6 +10,14 @@ from SRT_inline import *
 import time
 
 
+def timeStamp(message, file=None):
+    #
+    # This function provides a standard time-stamped output statement
+    #
+    timeStamp = time.strftime("[%H:%M:%S]")
+    print(timeStamp, message, file=file)
+
+
 SRT.connect(False)
 sats = load.tle_file('lib/ISS.tle')
 iss = sats[0]
