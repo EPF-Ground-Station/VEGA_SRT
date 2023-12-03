2fc411f863ebf6a77461ce49e95cce8710e0cce3
EPF-Ground-Station <epflgroundstation@gmail.com>
2023-12-03 18:52:44 +0100

    SDR changes

--- GS_interface/HackRFTest.py
+++ GS_interface/HackRFTest.py
@@ -3,6 +3,8 @@ from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CF32
 from SoapySDR import *
 import numpy
 
+sampleRate = 4e6
+freq = 1420e6
 print("loading results")
 results = SoapySDR.Device.enumerate()
 for result in results: print(result)
@@ -11,6 +13,14 @@ args = dict(driver="hackrf")
 sdr = SoapySDR.Device(args)
 
 print(sdr.listAntennas(SOAPY_SDR_RX, 0))
+listofGains=sdr.listGains(SOAPY_SDR_RX, 0)
 print(sdr.listGains(SOAPY_SDR_RX, 0))
+for i in listofGains:
+    print(sdr.getGainRange(SOAPY_SDR_RX,0,i))
+
 freqs = sdr.getFrequencyRange(SOAPY_SDR_RX, 0)
 for freqRange in freqs: print(freqRange)
+
+sdr.setSampleRate(SOAPY_SDR_RX, 0, sampleRate)           # Set sample rate
+#sdr.setGain(SOAPY_SDR_RX, 0, '', 14)        # Set the gain mode
+sdr.setFrequency(SOAPY_SDR_RX, 0, freq)          # Tune the LO
