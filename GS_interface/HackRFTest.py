b5613808f10061c7af5ea2b363bf7a8bc9ffc409
EPF-Ground-Station <epflgroundstation@gmail.com>
2023-12-03 18:27:18 +0100

    SDR changes

--- GS_interface/HackRFTest.py
+++ GS_interface/HackRFTest.py
@@ -6,3 +6,11 @@ import numpy
 print("loading results")
 results = SoapySDR.Device.enumerate()
 for result in results: print(result)
+
+args = dict(driver="hackrf")
+sdr = SoapySDR.Device(args)
+
+print(sdr.listAntennas(SOAPY_SDR_RX, 0))
+print(sdr.listGains(SOAPY_SDR_RX, 0))
+freqs = sdr.getFrequencyRange(SOAPY_SDR_RX, 0)
+for freqRange in freqs: print(freqRange)
