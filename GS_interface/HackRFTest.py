db6f3d8516b27e955e12f88cfe8569af9990ce45
EPF-Ground-Station <epflgroundstation@gmail.com>
2023-12-03 18:09:17 +0100

    SDR changes

--- GS_interface/HackRFTest.py
+++ GS_interface/HackRFTest.py
@@ -3,5 +3,6 @@ from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CF32
 from SoapySDR import *
 import numpy
 
+print("loading results")
 results = SoapySDR.Device.enumerate()
 for result in results: print(result)
