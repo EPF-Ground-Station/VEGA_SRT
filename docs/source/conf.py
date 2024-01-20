2d69d53392cd45fb6ae60e855f430ac84b2802fb
EPF-Ground-Station <epflgroundstation@gmail.com>
2024-01-20 23:46:54 +0100

    add readthedocs.yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ extensions = ["sphinx.ext.autodoc"]
 templates_path = ['_templates']
 exclude_patterns = []
 
-autodoc_mock_imports = ["virgo", "SoapySDR", "cv2"]
+autodoc_mock_imports = ["virgo", "SoapySDR", "cv2", "PySide6", "serial", "skyfield"]
 
 
 # -- Options for HTML output -------------------------------------------------
