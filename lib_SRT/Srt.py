75d5b01f39e791d469c7a51a2b5290fbd2437ca5
EPF-Ground-Station <epflgroundstation@gmail.com>
2024-02-06 17:21:24 +0100

    Working on client->server observations.

--- lib_SRT/Srt.py
+++ lib_SRT/Srt.py
@@ -1028,7 +1028,7 @@ class QObsProcess(QThread):
         raw_mode = self.raw_mode
 
         # If no indicated repository to save data
-        if repo is None or repo is '':
+        if repo is None or repo == '':
             # Make repo the default today's timestamp
             repo = datetime.today().strftime('%Y-%m-%d')
 
@@ -1047,7 +1047,7 @@ class QObsProcess(QThread):
         repo = repo + '/'
 
         # If no indicated observation name
-        if name is None or name is '':
+        if name is None or name == '':
             # Make the name to current timestamp
             name = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
