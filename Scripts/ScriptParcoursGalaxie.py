0ca4ca0dc2ddb6372dd7447ade44af98c8ae755d
Yannis Ulrich <yannis.ulrich2@gmail.com>
2024-05-19 14:37:52 +0200

    Meteo

--- Scripts/ScriptParcoursGalaxie.py
+++ Scripts/ScriptParcoursGalaxie.py
@@ -1,2 +1,26 @@
-#hello
-#hello2
+import requests
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+api_key = '6a00123049c113bc57044531717ed686'
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+# Give city name
+city_name = ('Ecublens,CH')
+
+# complete_url variable to store
+# complete url address
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+# get method of requests module
+# return response object
+response = requests.get(complete_url)
+
+# json method of response object
+# convert json format data into
+# python format data
+x = response.json()
+print(x)
+print(f"couverture de nuages: {x['clouds']['all']}%")
